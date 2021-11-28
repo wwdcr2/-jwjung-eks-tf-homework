ecf1e844ab20b33f4be8aab343c677a5b08f917f
jwjung <wwdcr2@gmail.com>
2021-11-28 15:42:41 +0900

    5th

--- userdata_k8s.sh
+++ userdata_k8s.sh
@@ -1,7 +1,7 @@
 curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
 curl -o kubectl.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl.sha256
 openssl sha1 -sha256 kubectl
-chmod +x ./kubectl
+chmod +x $HOME/kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --short --client
@@ -9,15 +9,15 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 sudo mv -v /tmp/eksctl /usr/local/bin
 
 aws configure set default.region ap-northeast-2
-aws configure set aws_access_key_id [AWS-AccessKey] 
-aws configure set aws_secret_access_key [AWS-SecretKey]
+aws configure set aws_access_key_id [ACCESS_KEY_ID]
+aws configure set aws_secret_access_key [SECRET_ACCESS_KEY]
 
 aws eks --region ap-northeast-2 update-kubeconfig --name eks-tf-demo-cluster
 
 aws eks describe-cluster --name eks-tf-demo-cluster --query "cluster.identity.oidc.issuer" --output text
 #-> https://oidc.eks.ap-northeast-2.amazonaws.com/id/19BE4D21B575A06856066CF1BCB74D5B
 
-aws iam list-open-id-connect-providers | grep 19BE4D21B575A06856066CF1BCB74D5B
+# aws iam list-open-id-connect-providers | grep 19BE4D21B575A06856066CF1BCB74D5B
 
 eksctl utils associate-iam-oidc-provider --cluster eks-tf-demo-cluster --approve
 
@@ -27,7 +27,7 @@ eksctl create iamserviceaccount \
   --cluster eks-tf-demo-cluster \
   --namespace kube-system \
   --name aws-load-balancer-controller \
-  --attach-policy-arn arn:aws:iam::713225129300:policy/AWSLoadBalancerControllerIAMPolicy \
+  --attach-policy-arn arn:aws:iam::$(aws sts get-caller-identity --output text --query Account):policy/AWSLoadBalancerControllerIAMPolicy \
   --override-existing-serviceaccounts \
   --approve
 
@@ -39,4 +39,3 @@ kubectl apply -f https://raw.githubusercontent.com/wwdcr2/-jwjung-eks-tf-homewor
 #kubectl get deployment -n kube-system aws-load-balancer-controller 
 
 kubectl apply -f https://raw.githubusercontent.com/wwdcr2/-jwjung-eks-tf-homework/master/file/2048.yaml
-
